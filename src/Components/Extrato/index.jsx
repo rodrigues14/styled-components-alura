428e88e1881474be13344091fd1ca6cf610be88f
Lucas Rodrigues <lukinhasr1423@gmail.com>
2022-12-21 18:40:49 -0300

    aula 4 finalizada

---
+++ src/Components/Extrato/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Box, Botao } from '../../Components/UI'
+import { extratoLista } from '../../info'
+import Items from '../items'
+
+export default function Extrato() {
+  return (
+    <Box>
+      {
+        extratoLista.updates.map(({ id, type, from, value, date }) => {
+          return (
+            <Items key={id} type={type} from={from} value={value} date={date} />
+          )
+        })
+      }
+      <Botao>Ver mais</Botao>
+    </Box>
+  )
+}
